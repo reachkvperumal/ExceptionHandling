98ab28bd10d8e554ccfe7c94297a07a1658fa06c
KV <indianakv@gmail.com>
2023-01-06 19:26:47 -0500

    OpenAPI demo added

--- src/test/java/com/kv/demo/jackson/demo/DemoApplicationTests.java
+++ src/test/java/com/kv/demo/jackson/demo/DemoApplicationTests.java
@@ -3,6 +3,8 @@ package com.kv.demo.jackson.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -12,5 +14,6 @@ class DemoApplicationTests {
     void contextLoads() {
         DemoApplication.main(new String[]{});
         assertTrue(true);
+        System.out.println(UUID.randomUUID());
     }
 }
